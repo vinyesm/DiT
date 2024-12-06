efda79c4befb1446d32d5e3fe12433f4f965f105
vinyesm <vinyes.marina@gmail.com>
2024-12-06 16:45:19 +0000

    desactivate ema

--- download.py
+++ download.py
@@ -28,7 +28,7 @@ def find_model(model_name):
         assert os.path.isfile(model_name), f'Could not find DiT checkpoint at {model_name}'
         checkpoint = torch.load(model_name, map_location=lambda storage, loc: storage)
         if "ema" in checkpoint:  # supports checkpoints from train.py
-            checkpoint = checkpoint["ema"]
+            checkpoint = checkpoint["model"]
         return checkpoint
 
 
